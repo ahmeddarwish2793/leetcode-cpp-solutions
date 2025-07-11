c995cb8c95d820847e4d237d450bb00eafeaeec4
ahmeddarwish2793 <ahmed.darwish2793@gmail.com>
2025-07-12 01:58:58 +0200

    Added Two Sum solution

---
+++ easy/two-sum.cpp
@@ -0,0 +1,23 @@
+// Problem: https://leetcode.com/problems/two-sum/
+// Level: Easy
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> index;
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i+1; j < nums.size(); j++) {
+                if(nums[i]+nums[j] == target) {
+                    index.push_back(i);
+                    index.push_back(j);
+                    return index;
+                }
+            }
+        }
+        return {};
+    }
+};
